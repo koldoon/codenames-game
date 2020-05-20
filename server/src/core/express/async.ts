798a90ae46418bdb430dbc281502ffe99f3b23a1
koldoon <ikoldoon@gmail.com>
2020-05-20 16:54:17 +0300

    [^] Ability to use sync handlers as well for "async" wrapper (to have errors wrapping as well)

--- server/src/core/express/async.ts
+++ server/src/core/express/async.ts
@@ -6,10 +6,13 @@ import { NextFunction, Request, Response } from 'express';
  * @param {(req: e.Request) => Promise<any>} handler
  * @returns {(req: e.Request, res: e.Response, next: e.NextFunction) => void}
  */
-export function async(handler: (req: Request) => Promise<any>) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        handler(req)
-            .then(result => res.json(result))
-            .catch(next);
+export function async(handler: (req: Request) => Promise<any> | any) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            res.json(await handler(req));
+        }
+        catch (e) {
+            next(e);
+        }
     }
 }
